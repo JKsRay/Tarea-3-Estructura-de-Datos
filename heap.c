9a72435c974852a68eed7583f35c0a9f92d18a78
JKsRay <127540914+JKsRay@users.noreply.github.com>
2023-05-20 22:39:24 +0000

    Creamos los structs necesarios para el programa, mostramos menu de opciones.

---
+++ heap.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "heap.h"
+
+typedef struct nodo{
+   void* data;
+   int priority;
+}heapElem;
+
+typedef struct Heap{
+  heapElem* heapArray;
+  int size;
+  int capac;
+} Heap;
+
+
+void* heap_top(Heap* pq){
+  if(pq->size == 0) return NULL;
+  
+    return pq->heapArray[0].data;
+}
+
+
+
+void heap_push(Heap* pq, void* data, int priority) {
+  for (int i = 0; i < pq->size; i++) {
+    if (pq->heapArray[i].priority == priority && pq->heapArray[i].data == data) {
+      return;
+    }
+  }
+
+  if (pq->size + 1 == pq->capac) {
+    pq->capac = pq->capac * 2 + 1;
+    pq->heapArray = (heapElem*)realloc(pq->heapArray, sizeof(heapElem) * pq->capac);
+  }
+
+  int indice = pq->size;
+
+  pq->heapArray[pq->size].data = data;
+  pq->heapArray[pq->size].priority = priority;
+
+  while (pq->heapArray[indice].priority < pq->heapArray[(indice - 1) / 2].priority) {
+    heapElem aux = pq->heapArray[(indice - 1) / 2];
+    pq->heapArray[(indice - 1) / 2] = pq->heapArray[indice];
+    pq->heapArray[indice] = aux;
+    indice = (indice - 1) / 2;
+  }
+
+  pq->size++;
+}
+
+
+
+
+void heap_pop(Heap* pq) {
+  if (pq->size == 0) {
+    return;
+  }
+
+  if (pq->size == 1) {
+    pq->size--;
+    return;
+  }
+
+  pq->heapArray[0] = pq->heapArray[pq->size - 1];
+  pq->size--;
+
+  int indicePadre = 0;
+  int indiceHijoIzq = 1;
+  int indiceHijoDer = 2;
+
+  while (indiceHijoIzq < pq->size) {
+    int indiceHijoMenor = indiceHijoIzq;
+    if (indiceHijoDer < pq->size && pq->heapArray[indiceHijoDer].priority < pq->heapArray[indiceHijoIzq].priority) {
+      indiceHijoMenor = indiceHijoDer;
+    }
+
+    if (pq->heapArray[indicePadre].priority <= pq->heapArray[indiceHijoMenor].priority){
+      break;
+    }
+
+    heapElem aux = pq->heapArray[indicePadre];
+    pq->heapArray[indicePadre] = pq->heapArray[indiceHijoMenor];
+    pq->heapArray[indiceHijoMenor] = aux;
+
+    indicePadre = indiceHijoMenor;
+    indiceHijoIzq = (indiceHijoIzq + 1) * 2 - 1;
+    indiceHijoDer = (indiceHijoDer + 1) * 2 - 1;
+  }
+}
+
+
+Heap* createHeap(){
+  Heap * newHeap = (Heap*) malloc(sizeof(Heap));
+  if(newHeap == NULL) EXIT_FAILURE;
+
+  newHeap->size = 0;
+  newHeap->heapArray = (heapElem*) malloc(sizeof(heapElem)*3);
+  newHeap->capac = 3;
+  
+   return newHeap;
+}
--- main.c
+++ main.c
@@ -1,7 +1,106 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h> 
+#include "heap.h"
+#include "stack.h"
+#include "Map.h"
 
-int main(void) {
-  printf("Hello World\n");
-  printf("Hello World\n");
-  return 0;
+typedef struct{
+    char nombre[21];
+    bool explorado;
+    int prioridad;
+    List * listaPrecedentes;
+}TareaNodo;
+
+typedef struct{
+    char nombre[21];
+    bool visitado;
+    int prioridad;
+}TareaNodoPrecedente;
+
+typedef struct{
+    char nombreAccion[51];
+    TareaNodo * tarea;
+    TareaNodoPrecedente * tareaPrecedente;
+    List* listaTemporal;
+}TareaNodoPila;
+
+int is_equal_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2)==0) return 1;
+    return 0;
+}
+int lower_than_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2) < 0) return 1;
+    return 0;
+}
+int is_equal_int(void * key1, void * key2) {
+    if(*(int*)key1 == *(int*)key2) return 1;
+    return 0;
+}
+int lower_than_int(void * key1, void * key2) {
+    if(*(int*)key1 < *(int*)key2) return 1;
+    return 0;
+}
+
+int main() {
+  char opcion[11];
+  bool ejecucion = true;
+
+  TareaNodo *tarea;
+  Map * mapaTareas = createMap(is_equal_string);
+  Stack*pilaAcciones = stack_create();
+  
+  while (ejecucion) {
+    printf("\n1.-AGREGAR TAREA\n");
+    printf("2.-ESTABLECER PRECEDENCIA ENTRE TAREAS\n");
+    printf("3.-MOSTRAR TAREAS POR HACER\n");
+    printf("4.-MARCAR TAREA COMO COMPLETADA\n");
+    printf("5.-DESHACER ULTIMA ACCION\n");
+    printf("6.-CARGAR DATOS DE TAREAS DE UN ARCHIVO\n");
+
+    printf("PRESIONE 0 PARA SALIR\n\n");
+
+    scanf("%10s[^\n]", opcion);
+    getchar();
+
+    //Se comprueba que la opcion ingresada sea un número
+    if (isdigit(opcion[0])) {
+      int opcionNumero = atoi(opcion);
+
+      switch (opcionNumero) {
+      case 1:
+        break;
+
+      case 2:
+        break;
+
+      case 3:
+        break;
+
+      case 4:
+        break;
+
+      case 5:
+        break;
+
+      case 6:
+        break;
+        
+      case 0:
+        printf("QUE TENGA BUEN DÍA, ADIÓS\n");
+        ejecucion = false;
+        break;
+
+      default:
+        printf("\nSELECCIONE UNA OPCIÓN VÁLIDA\n\n");
+        break;
+      }
+    } else {
+      if (isalpha(opcion[0]))
+        printf("\nSELECCIONE UNA OPCIÓN VÁLIDA\n\n");
+    }
+  }
+    return 0;
 }
